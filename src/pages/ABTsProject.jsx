cba7b218b13c3b7e1e383b221bf12b56c1e20313
mar69287 <marcoa.ruiz97@gmail.com>
2024-05-21 20:51:49 -0700

    update pdf interaction

--- src/pages/ABTsProject.jsx
+++ src/pages/ABTsProject.jsx
@@ -6,51 +6,21 @@ import { MdOutlineKeyboardArrowDown, MdFilterList, MdKeyboardBackspace, MdOutlin
 import { createABT } from "../utilities/Contract";
 import { useNavigate } from "react-router-dom";
 import Upload from '../pdfUpload/Upload'
+import MintABT from "./Minting/MintABT";
 
 const ABTsProject = ({client}) => {
   const [myABTs, setMyABTs] = useState([])
   const [abtFilter, setAbtFilter] = useState('Yearly')
   const [openMint, setOpenMint] = useState(false)
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
   const [pdfFile, setPdfFile] = useState({});
   const [uploaded, setUploaded] = useState(null);
-  const [loading, setLoading] = useState(false)
-
-  const handleMinting = async (e) => {
-    e.preventDefault();
-    const formData = new FormData(event.target);
-    const name = formData.get("name");
-    const description = formData.get("description");
-    const pdf = formData.get("pdf");
-
-    const data = {
-        "user_address": client.account,
-        "network": client.chainId,
-        "metadata": {
-            "name": name,
-            "description": description, 
-            "external_url": "http://localhost:5173/", 
-            "image": "https://cdn.osxdaily.com/wp-content/uploads/2016/09/search-preview-mac-pdf-1.jpg", 
-            "document": "https://drive.google.com/file/d/12AOwLvCp_rb5d4dCutzOmMShV3md0xng/view?usp=sharing"
-        }
-    }
-
-    // console.log(data)
-
-        try {
-            const response = await createABT(data);
-            const tokenId = response.tokenId
-            console.log(typeof tokenId)
-            console.log(tokenId)
-            navigate(`/abt/${tokenId}`);
-          } catch (error) {
-            console.error('Error:', error);
-          }
-    }
+  // const [loading, setLoading] = useState(false);
 
   if (openMint) {
     return (
-      <MintPage setOpenMint={setOpenMint} handleMinting={handleMinting} setPdfFile={setPdfFile} pdfFile={pdfFile} setUploaded={setUploaded} uploaded={uploaded} />
+      // <MintPage setOpenMint={setOpenMint} handleMinting={handleMinting} setPdfFile={setPdfFile} pdfFile={pdfFile} setUploaded={setUploaded} uploaded={uploaded} />
+      <MintABT client={client} setOpenMint={setOpenMint} setPdfFile={setPdfFile} pdfFile={pdfFile} />
     )
   }
 
@@ -96,7 +66,7 @@ const ABTsProject = ({client}) => {
                 <p className="text-center text-base">Create a dynamic token in NIOVLABS</p>
                 <p className="text-center text-base leading-5">Create a new ABT with multiple interchangeable layers</p>
               </div>
-              <button onClick={() => setOpenMint(true)} className="w-full sm:w-max sm:px-5 py-2 gap-3 rounded=sm flex justify-center items-center bg-[#7B61FF] text-white">
+              <button onClick={() => setOpenMint(true)} className="w-full sm:w-max sm:px-5 py-2 gap-3 rounded=sm flex justify-center items-center bg-primary1 text-white">
                 <div className='text-md lg:text-sm'>
                     <FaPen />
                 </div>
@@ -113,80 +83,80 @@ const ABTsProject = ({client}) => {
 
 export default ABTsProject
 
-const MintPage = ({ setOpenMint, handleMinting, pdfFile, setPdfFile, setUploaded, uploaded}) => {
-  return (
-    <>
-      <PageHeader title={'ABTs Projects'} />
-      <div className="w-full p-5 lg:py-7 lg:px-10 grid grid-cols-1 lg:grid-cols-2 lg:gap-x-10 2xl:gap-x-16">
-        <button onClick={() => setOpenMint(false)} className={`w-max lg:col-span-2 gap-2 mb-2 rounded flex justify-start items-center  text-black text-base md:text-base`}>
-              <div className="text-sm">
-                <MdArrowBackIos />
-              </div>
-              Back to Projects
-        </button>
-        <div className="w-full pb-3 lg:col-span-2">
-          <h1 className="text-xl font-semibold mb-1">Create an ABT</h1>
-          <p className="leading-5">Once your item has been minted, you will not be able to change any of its information</p>
-        </div>
-        <div className="bg-slate-100 w-48 h-44 md:w-72 md:h-80 lg:w-full lg:h-full 2xl:h-[45rem] min-[1700px]:h-[55rem] border-slate-400 border-[1px] flex flex-col justify-center items-center gap-0 mb-5">
-          <div className='text-2xl lg:text-3xl'>
-            <MdOutlineRemoveRedEye />
-          </div>
-          <p className="">Live preview</p>
-        </div>
-        <form className="w-full" onSubmit={handleMinting}>
-          <div className="mb-3 flex flex-col justify-start items-start ">
-            <label className="text-gray-700 text-md mb-2 lg:mb-3 font-medium lg:leading-3" htmlFor="name">
-              Name
-            </label>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="Name"
-              name="name"
-              type="text"
-            />
-          </div>
-          {/* <div className="mb-3 flex flex-col justify-start items-start ">
-            <label className="text-gray-700 text-md mb-2 font-medium" htmlFor="symbol">
-              Symbol
-            </label>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="Symbol"
-              name="symbol"
-              type="text"
-            />
-          </div> */}
-          {/* <div className="mb-3 flex flex-col justify-start items-start ">
-            <label className="text-gray-700 text-md mb-2 font-medium" htmlFor="pdf">
-              Contract document
-            </label>
-            <label className="w-full flex justify-center gap-1 items-center bg-white text-blue rounded shadow tracking-wide border border-blue cursor-pointer py-[7px]">
-              <span className="text-base leading-normal">Add document</span>
-              <span className="text-base leading-normal text-slate-500">(pdf)</span>
-              <input type='file' id="pdf" accept=".pdf" name="pdf" className="hidden" onChange={handlePDF}/>
-            </label>
-          </div> */}
-          <Upload pdfFile={pdfFile} setPdfFile={setPdfFile} setUploaded={setUploaded} uploaded={uploaded} />
-          <div className="mb-3 flex flex-col justify-start items-start ">
-            <label className="text-gray-700 text-md mb-2 font-medium" htmlFor="description">
-              Description
-            </label>
-            <textarea
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="description"
-              name="description"
-              rows="4"
-            />
-          </div>
-          <button
-            type="submit"
-            className="mt-4 w-full bg-[#7B61FF] text-white font-medium py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-          >
-            Mint Asset
-          </button>
-        </form>
-      </div>
-    </>
-  )
-}
+// const MintPage = ({ setOpenMint, handleMinting, pdfFile, setPdfFile, setUploaded, uploaded}) => {
+//   return (
+//     <>
+//       <PageHeader title={'ABTs Projects'} />
+//       <div className="w-full p-5 lg:py-7 lg:px-10 grid grid-cols-1 lg:grid-cols-2 lg:gap-x-10 2xl:gap-x-16">
+//         <button onClick={() => setOpenMint(false)} className={`w-max lg:col-span-2 gap-2 mb-2 rounded flex justify-start items-center  text-black text-base md:text-base`}>
+//               <div className="text-sm">
+//                 <MdArrowBackIos />
+//               </div>
+//               Back to Projects
+//         </button>
+//         <div className="w-full pb-3 lg:col-span-2">
+//           <h1 className="text-xl font-semibold mb-1">Create an ABT</h1>
+//           <p className="leading-5">Once your item has been minted, you will not be able to change any of its information</p>
+//         </div>
+//         <div className="bg-slate-100 w-48 h-44 md:w-72 md:h-80 lg:w-full lg:h-full 2xl:h-[45rem] min-[1700px]:h-[55rem] border-slate-400 border-[1px] flex flex-col justify-center items-center gap-0 mb-5">
+//           <div className='text-2xl lg:text-3xl'>
+//             <MdOutlineRemoveRedEye />
+//           </div>
+//           <p className="">Live preview</p>
+//         </div>
+//         <form className="w-full" onSubmit={handleMinting}>
+//           <div className="mb-3 flex flex-col justify-start items-start ">
+//             <label className="text-gray-700 text-md mb-2 lg:mb-3 font-medium lg:leading-3" htmlFor="name">
+//               Name
+//             </label>
+//             <input
+//               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+//               id="Name"
+//               name="name"
+//               type="text"
+//             />
+//           </div>
+//           {/* <div className="mb-3 flex flex-col justify-start items-start ">
+//             <label className="text-gray-700 text-md mb-2 font-medium" htmlFor="symbol">
+//               Symbol
+//             </label>
+//             <input
+//               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+//               id="Symbol"
+//               name="symbol"
+//               type="text"
+//             />
+//           </div> */}
+//           {/* <div className="mb-3 flex flex-col justify-start items-start ">
+//             <label className="text-gray-700 text-md mb-2 font-medium" htmlFor="pdf">
+//               Contract document
+//             </label>
+//             <label className="w-full flex justify-center gap-1 items-center bg-white text-blue rounded shadow tracking-wide border border-blue cursor-pointer py-[7px]">
+//               <span className="text-base leading-normal">Add document</span>
+//               <span className="text-base leading-normal text-slate-500">(pdf)</span>
+//               <input type='file' id="pdf" accept=".pdf" name="pdf" className="hidden" onChange={handlePDF}/>
+//             </label>
+//           </div> */}
+//           <Upload pdfFile={pdfFile} setPdfFile={setPdfFile} setUploaded={setUploaded} uploaded={uploaded} />
+//           <div className="mb-3 flex flex-col justify-start items-start ">
+//             <label className="text-gray-700 text-md mb-2 font-medium" htmlFor="description">
+//               Description
+//             </label>
+//             <textarea
+//               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+//               id="description"
+//               name="description"
+//               rows="4"
+//             />
+//           </div>
+//           <button
+//             type="submit"
+//             className="mt-4 w-full bg-[#7B61FF] text-white font-medium py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+//           >
+//             Mint Asset
+//           </button>
+//         </form>
+//       </div>
+//     </>
+//   )
+// }
---
+++ src/pages/Minting/MintABT.jsx
@@ -0,0 +1,144 @@
+import { useState } from "react";
+import PageHeader from "../../components/PageHeader"
+import Upload from "../../pdfUpload/Upload"
+import { MdOutlineRemoveRedEye, MdArrowBackIos } from "react-icons/md";
+import { createABT } from "../../utilities/Contract";
+import { useNavigate } from "react-router-dom";
+
+const pdfURL = 'http://localhost:3000'
+
+const MintABT = ({ client, setOpenMint, pdfFile, setPdfFile }) => {
+    const [name, setName] = useState('');
+    const [description, setDescription] = useState('');
+    const [uploadedPDF, setUploadedPDF] = useState(null);
+    const [pdfFilePath, setPdfFilePath] = useState('');
+    const navigate = useNavigate();
+
+    const handleMinting = async (e) => {
+        e.preventDefault();
+
+        if (!name  || !uploadedPDF) return;
+        const formData = new FormData(event.target);
+        const abtName = formData.get("name");
+        const description = formData.get("description");
+    
+        const data = {
+            "user_address": client.account,
+            "network": client.chainId,
+            "metadata": {
+                "name": abtName,
+                "description": description, 
+                "external_url": "http://localhost:5173/", 
+                "image": "https://cdn.osxdaily.com/wp-content/uploads/2016/09/search-preview-mac-pdf-1.jpg", 
+                "document": "https://drive.google.com/file/d/12AOwLvCp_rb5d4dCutzOmMShV3md0xng/view?usp=sharing"
+            }
+        }
+  
+    
+        try {
+            const response = await createABT(data);
+            const tokenId = response.tokenId
+            // console.log(typeof tokenId)
+            // console.log(tokenId)
+            navigate(`/abt/${tokenId}`);
+          } catch (error) {
+            console.error('Error:', error);
+        }
+    }
+
+  return (
+    <>
+    <PageHeader title={'Mint Page'} />
+    <div className="w-full p-5 lg:py-7 lg:px-10 grid grid-cols-1 lg:grid-cols-2 lg:gap-x-10 2xl:gap-x-16">
+      <button onClick={() => setOpenMint(false)} className={`w-max lg:col-span-2 gap-2 mb-2 rounded flex justify-start items-center  text-black text-base md:text-base`}>
+            <div className="text-sm">
+              <MdArrowBackIos />
+            </div>
+            Back to Projects
+      </button>
+      <div className="w-full pb-3 lg:col-span-2">
+        <h1 className="text-xl font-semibold mb-1">Create an ABT</h1>
+        <p className="leading-5">Once your item has been minted, you will not be able to change any of its information</p>
+      </div>
+      <div className="bg-slate-100 w-48 h-44 md:w-72 md:h-80 lg:w-full lg:h-full 2xl:h-[45rem] min-[1700px]:h-[55rem] border-slate-400 border-[1px] flex flex-col justify-center items-center gap-0 mb-5">
+        {
+          pdfFilePath ? (
+            <div className="flex flex-col items-center justify-center gap-2">
+              <a href={`${pdfURL}${pdfFilePath}`} target="_blank" rel="noopener noreferrer" className="p-2 w-32 grid place-content-center bg-accent1 font-medium text-white rounded">Preview Now</a>
+              <button className="p-2 w-32 grid place-content-center bg-accent3 font-medium text-white rounded">
+                Cancel
+              </button>
+            </div>
+          ) : (
+            <>
+              <div className='text-2xl lg:text-3xl'>
+                <MdOutlineRemoveRedEye />
+              </div>
+              <p className="">Live preview</p>
+
+            </>
+          )
+        }
+      </div>
+      <form className="w-full" onSubmit={handleMinting}>
+        <div className="mb-3 flex flex-col justify-start items-start ">
+          <label className="text-gray-700 text-md mb-2 lg:mb-3 font-medium lg:leading-3" htmlFor="name">
+            Name
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="Name"
+            name="name"
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        {/* <div className="mb-3 flex flex-col justify-start items-start ">
+          <label className="text-gray-700 text-md mb-2 font-medium" htmlFor="symbol">
+            Symbol
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="Symbol"
+            name="symbol"
+            type="text"
+          />
+        </div> */}
+        {/* <div className="mb-3 flex flex-col justify-start items-start ">
+          <label className="text-gray-700 text-md mb-2 font-medium" htmlFor="pdf">
+            Contract document
+          </label>
+          <label className="w-full flex justify-center gap-1 items-center bg-white text-blue rounded shadow tracking-wide border border-blue cursor-pointer py-[7px]">
+            <span className="text-base leading-normal">Add document</span>
+            <span className="text-base leading-normal text-slate-500">(pdf)</span>
+            <input type='file' id="pdf" accept=".pdf" name="pdf" className="hidden" onChange={handlePDF}/>
+          </label>
+        </div> */}
+        <Upload pdfFile={pdfFile} setPdfFile={setPdfFile} setUploaded={setUploadedPDF} uploaded={uploadedPDF} name={name} setPdfFilePath={setPdfFilePath} />
+        <div className="mb-3 flex flex-col justify-start items-start ">
+          <label className="text-gray-700 text-md mb-2 font-medium" htmlFor="description">
+            Description
+          </label>
+          <textarea
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="description"
+            name="description"
+            rows="4"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </div>
+        <button
+          type="submit"
+          className="mt-4 w-full bg-primary1 text-white font-medium py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        >
+          Mint Asset
+        </button>
+      </form>
+    </div>
+  </>
+  )
+}
+
+export default MintABT

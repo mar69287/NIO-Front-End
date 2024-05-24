c7c704f752014e13c5879d01e969dd9c34de3f41
mar69287 <marcoa.ruiz97@gmail.com>
2024-05-24 12:09:09 -0700

    update css

--- src/components/ActiveSection.jsx
+++ src/components/ActiveSection.jsx
@@ -28,7 +28,7 @@ const Tab = ({ selected, setSelected, title, tabNum}) => {
                 className="relative z-0 flex w-full items-center justify-center border-b-4 border-slate-200 bg-white p-3 transition-colors hover:bg-slate-100 md:flex-col"
             >
                 <span
-                    className={`min-w-[100px] max-w-[200px] text-start text-sm text-slate-600 transition-opacity md:text-center ${
+                    className={`w-[80px] lg:w-[100px] text-center text-sm text-slate-600 transition-opacity md:text-center ${
                         selected ? "opacity-100" : "opacity-50"
                     }`}
                 >
